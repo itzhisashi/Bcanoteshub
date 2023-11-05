a69a2b3789e250d9ffb799459a6669c933da2dc6
Mr. Aurgon <80805078+aurgongaming@users.noreply.github.com>
2023-11-06 00:18:50 +0530

    Add files via upload

---
+++ data/sem1/assets/rd.js
@@ -0,0 +1,16 @@
+function animateValue(obj, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+
+const obj = document.getElementById("value");
+animateValue(obj, 25, 0, 26000);
+
