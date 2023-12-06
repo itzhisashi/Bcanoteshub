d69021844f02e75f208b447702583a3f78185e9c
Mr. Aurgon <80805078+aurgongaming@users.noreply.github.com>
2023-12-06 11:49:59 +0530

    Add files via upload

---
+++ data/sem1/assets/rd.js
@@ -0,0 +1,33 @@
+function animateValue(obj, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+
+const obj = document.getElementById("value");
+animateValue(obj, 30, 0, 31000);
+
+/*------- Show element -------*/
+const el = document.getElementById('loader');
+
+setTimeout(() => {
+  el.style.display = 'block';
+}, 31000);
+
+/*------- Hide element-------*/
+setTimeout(() => {
+  const box = document.getElementById('time-b');
+
+  // 👇️ removes element from DOM
+  box.style.display = 'none';
+
+  // 👇️ hides element (still takes up space on page)
+  // box.style.visibility = 'hidden';
+}, 31000);
